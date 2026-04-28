4f8507591b2c23d28215f8358450e5696820f66e
dayat@Bounty <stu2501261018@uni-plovdiv.bg>
2026-04-28 21:06:46 +0300

    sum of numbers task added

---
+++ Week6/Lecture6/src/SumNumbers.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int beginning = Integer.parseInt(sc.nextLine());
+        int ending = Integer.parseInt(sc.nextLine());
+        int magicNumber = Integer.parseInt(sc.nextLine());
+
+        int counter = 0;
+
+        boolean found = false;
+
+        int i = beginning;
+        int j = beginning;
+
+        for(i = beginning; i <= ending; i++){
+            for(j = beginning; j <= i; j++){
+                counter++;
+
+                if(i + j == magicNumber){
+                    found = true;
+                    break;
+                }
+            }
+            if(found){
+                break;
+            }
+        }
+        if(found){
+            System.out.printf("Combination N: %d (%d + %d = %d)", counter, i, j, magicNumber);
+        }else{
+            System.out.printf("%d combinations - neither equals %d", counter, magicNumber);
+        }
+
+    }
+}
